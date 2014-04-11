ab40ea282042edc7d91490bca3165af0fa2d3f93
Robert Graves <robertjgraves@gmail.com>
2014-04-11 14:41:28 -0700

    setup EF to initialize the database with test data

--- ContosoUniversity/ContosoUniversity/DAL/SchoolContext.cs
+++ ContosoUniversity/ContosoUniversity/DAL/SchoolContext.cs
@@ -13,7 +13,7 @@ namespace ContosoUniversity.DAL
 
         public DbSet<Student> Students { get; set; }
         public DbSet<Enrollment> Enrollments { get; set; }
-        public DbSet<Course> Course { get; set; }
+        public DbSet<Course> Courses { get; set; }
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
